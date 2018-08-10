fed9bf4d43f987610fd6430d016fed998cf47acf
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-08-10 07:49:57 +0700

    Update svs.js

--- cmds/svs.js
+++ cmds/svs.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (bot, msg, args) => {
+module.exports.run = async (bot, message, args) => {
 
 let msg = bot.guilds.map(guild => `**${guild.name}** Member ${guild.memberCount}`
 let embed = new Discord.RichEmbed()
 .setTitle(`i'm in ${bot.guild.size} Guilds!`)
 .setDescription(`${msg}`)
 .setColor('GREEN')
-msg.channel.send(embed);
+message.channel.send(embed);
 }
