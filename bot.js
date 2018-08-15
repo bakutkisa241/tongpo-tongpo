3bed9367802dfff15fbae7d023c6e40cbc5f8eed
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-08-15 16:11:31 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,35 +153,35 @@ bot.on("message", async message => {
     };
   }
 
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
+let xpAdd = Math.floor(Math.random() * 7) + 8;
+console.log(xpAdd);
 
-  if(!xp[message.author.id]){
+if (!xp[message.author.id]) {
     xp[message.author.id] = {
-      xp: 0,
-      level: 1
+        xp: 0,
+        level: 1
     };
-  }
+}
 
 
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
+let curxp = xp[message.author.id].xp;
+let curlvl = xp[message.author.id].level;
+let nxtLvl = xp[message.author.id].level * 300;
+xp[message.author.id].xp = curxp + xpAdd;
+if (nxtLvl <= xp[message.author.id].xp) {
     xp[message.author.id].level = curlvl + 1;
     let lvlup = new Discord.RichEmbed()
-    .setTitle("Level Up!")
-    .setAuthor(`Keep going ${message.author.username}!`)
-    .setColor(0x32d732)
-    .addField("Unlock Level", curlvl + 1)
-    .setFooter("you can see your xp/level/rank by doing b!level","https://cdn.discordapp.com/avatars/471150809196003328/a0ed47f2512655b5604a94e0cfb950ef.png?size=2048")
-    .setTimestamp()
+        .setTitle("Level Up!")
+        .addField("Congrats to", `${message.author}`)
+        .setColor("#08ff00")
+        .addField("New Level", curlvl + 1);
 
-message.channel.send(lvlup)
-
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
+    message.channel.send(lvlup).then(msg => {
+        msg.delete(5000)
+    });
+}
+fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+            if (err) console.log(err)
   });
 });
 bot.on("guildMemberRemove", async (member, client, message, args, level) => {  
