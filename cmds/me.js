6bb75421e46b8fc3ea668ca6c18de8a7071b1c1a
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-08-23 17:48:27 +0700

    Update me.js

--- cmds/me.js
+++ cmds/me.js
@@ -1,18 +1,19 @@
 const Discord = require("discord.js");
-let xp = require("../xp.json");
-let coins = require("../coins.json");
+const xp = require("../xp.json");
+const coins = require("../coins.json");
+let user = message.members.mentions.first() || message.atuhor.id;
 module.exports.run = async (bot, message, args) => {
                  message.react("✅")
 
 
-  if(!xp[message.author.id]){
+  if(!xp[user.id]){
    message.channel.send(`Created New profile for user ${message.author.id}. Please retype ~profile`);    
    xp[message.author.id] = {
      xp: 0,
      level: 1
   };
 }
-    if(!coins[message.author.id]){
+    if(!coins[user.id]){
     coins[message.author.id] = {
       coins: 0
     };
@@ -20,13 +21,13 @@ module.exports.run = async (bot, message, args) => {
   
   let coinss = coins[message.author.id].coins;
   let uicon = message.author.displayAvatarURL;
-  let userxp = xp[message.author.id].xp;
-  let userlvl = xp[message.author.id].level;
+  let userxp = xp[user.id].xp;
+  let userlvl = xp[user.id].level;
   let nextLvlXp = userlvl * 1000;
   let difference = nextLvlXp - userxp;
   let profileEmbed = new Discord.RichEmbed()
-  .setAuthor(`${message.author.username}'s Profile`)
-  .setDescription(`${message.author.username} has not yet set a bio`)
+  .setAuthor(`${user.tag}'s Profile`)
+  .setDescription(`${user.tag} has not yet set a bio`)
   .setColor("#32d732")
   .setThumbnail(uicon)
   .addField("Global Level", userlvl, true)
